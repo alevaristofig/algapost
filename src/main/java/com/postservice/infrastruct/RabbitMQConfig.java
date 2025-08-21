4a0cf585b06c5a9a93353a4b794582c2ea4b4bc4
Alexandre <alevaristofig@gmail.com>
2025-08-20 21:47:38 -0300

    Implementação da classe RabbitMQConfig, com as configurações das exchanges e filas

--- src/main/java/com/postservice/infrastruct/RabbitMQConfig.java
+++ src/main/java/com/postservice/infrastruct/RabbitMQConfig.java
@@ -1,7 +1,11 @@
 package com.postservice.infrastruct;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -10,12 +14,13 @@ import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+
 @Configuration
 public class RabbitMQConfig {
 
 	private static final String TEXT_PROCESSOR = "text-processor-service.post-processing.v1";
 	private static final String FANOUT_TEXT_PROCESSOR = "text-processor-service.post-processing.v1"+".e";
-	private static final String QUEU_TEXT_PROCESSOR = "text-processor-service.post-processing.v1"+".q";
+	private static final String QUEUE_TEXT_PROCESSOR = "text-processor-service.post-processing.v1"+".q";
 	
 	@Bean
 	public Jackson2JsonMessageConverter jackson2JsonMessageConverter(ObjectMapper mapper) {
@@ -30,5 +35,15 @@ public class RabbitMQConfig {
 	@Bean
 	public FanoutExchange exchange() {
 		return ExchangeBuilder.fanoutExchange(FANOUT_TEXT_PROCESSOR).build();
+	}
+	
+	@Bean
+	public Queue queueText() {
+		return QueueBuilder.durable(QUEUE_TEXT_PROCESSOR).build();
+	}
+	
+	@Bean
+	public Binding bindingText() {
+		return BindingBuilder.bind(queueText()).to(exchange());
 	}
 }
