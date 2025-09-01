1052252f54f78dd4e34ec6655d7b5c3730f21da4
Alexandre <alevaristofig@gmail.com>
2025-09-01 18:44:10 -0300

    Implementação do repositorio post

---
+++ src/main/java/com/postservice/domain/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.postservice.domain.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.postservice.domain.model.Post;
+
+public interface PostRepository extends MongoRepository<Post, UUID>{
+
+}
